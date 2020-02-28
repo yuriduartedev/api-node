d75ae1c38eb2b59c24ebfce27e8ca842b596a489
Yuri Duarte <yuriduartedev@gmail.com>
2020-02-28 11:28:19 -0300

    Finaliza o CRUD e adiciona paginação a API.

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,9 @@ const ProductController = require('./controllers/ProductController');
 
 // Primeira rota
 routes.get('/products', ProductController.index);
+routes.get('/products/:id', ProductController.show);
 routes.post('/products', ProductController.store);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.destroy);
 
 module.exports = routes;
