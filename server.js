433fb8443c930ec70fb8e645ee178e755e2d5526
Yuri Duarte <yuriduartedev@gmail.com>
2020-02-28 11:31:28 -0300

    Adiciona CORS ao projeto

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
-const requireDir = require('require-dir')
+const requireDir = require('require-dir');
 
 // Iniciando o App
 const app = express();
 // Permitir que eu envie dados para aplicação no formato JSON
 app.use(express.json());
+// Permite que outros domínios acessem a sua API.
+app.use(cors());
 
 // Iniciando o DB
 mongoose.connect(
--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -6,7 +6,6 @@ module.exports = {
   async index(req, res) {
     const { page = 1 } = req.query;
     const products = await Product.paginate({}, { page, limit: 10 });
-
     return res.json(products);
   },
 
